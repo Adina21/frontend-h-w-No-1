ddda3c5b6983956f9d1c2d802cb0f04ce29c7413
Adina21 <adina.ulfanova@gmail.com>
2024-02-12 12:39:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let birth_year = +prompt('write your birth year')
+// let val = +prompt('input')
+
+age = 2024 - birth_year
+
+if (0 < age < 30) {
+     alert('you are young');
+} else if (30 < age < 60) {
+    alert('middle age crisis');
+} else if (30 < age < 60) {
+    alert('middle age crisis');
+} else if (age < 60) {
+    alert('вам советуется посетить доктора');
+} 
+
+if (birth_year >= 2024 ) {
+    alert('вы еще не родились, ждите своего очереди :))')
+}
+// условии работают не корректно
+
+
+// dop zadaniye
+// if (Number.isInteger(val) ) {
+//     alert('вы ввели число')
+
+// } else {
+//     alert('введено не корректное значение')
+// }
